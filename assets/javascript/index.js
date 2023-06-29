e645971f54de61828a8f52fd4f4f4b248ed7044e
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-06-29 01:47:12 -0500

    starting game grid and animation for canvas

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,9 +3,15 @@ const ctx = canvas.getContext('2d');
 canvas.width = 900;
 canvas.height = 600;
 
+const cellSize = 100;
+const cellGap = 3;
+const gameGrid = [];
 
 // grid board
-
+const controlBar = {
+    width: canvas.width,
+    height: cellSize,
+}
 // projectiles/attacks
 
 // towers
@@ -15,3 +21,10 @@ canvas.height = 600;
 // resources
 
 // utils
+function animate() {
+    ctx.fillStyle= 'blue';
+    ctx.fillRect(0,0, controlBar.width, controlBar.height);
+    requestAnimationFrame(animate);
+}
+
+animate();
