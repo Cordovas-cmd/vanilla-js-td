80e6e932eaf58ad6c92e13284ffe17dd9454fcc9
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-06-29 01:57:02 -0500

    set up game grid cells

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -12,6 +12,37 @@ const controlBar = {
     width: canvas.width,
     height: cellSize,
 }
+
+class Cell {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+    }
+    draw() {
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+
+function createGrid() {
+    for (let y = cellSize; y < canvas.height; y += cellSize) {
+        for (let x = 0; x < canvas.width; x += cellSize) {
+            gameGrid.push(new Cell(x, y));
+        }
+    }
+}
+
+createGrid();
+
+
+function handleGameGrid() {
+    for (let i = 0; i < gameGrid.length; i++) {
+        gameGrid[i].draw();
+    }
+}
 // projectiles/attacks
 
 // towers
@@ -22,9 +53,10 @@ const controlBar = {
 
 // utils
 function animate() {
-    ctx.fillStyle= 'blue';
-    ctx.fillRect(0,0, controlBar.width, controlBar.height);
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, controlBar.width, controlBar.height);
     requestAnimationFrame(animate);
+    handleGameGrid();
 }
 
 animate();
